4b0dd264944f3245f3316f1fd7c9b07b1855f43d
Thomas Charbit <thomas.Charbit@june22.eu>
2014-08-25 15:05:09 +0200

    html ready for minified file, gulpfile fixed

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,14 @@ Todo:
 - conditional builld base on dev or prod arg
 */
 
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    compass = require('gulp-compass'),
-    path = require('path'),
+var gulp         = require('gulp'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    rename       = require('gulp-rename'),
+    compass      = require('gulp-compass'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    sourcemaps = require('gulp-sourcemaps');
+    minifycss    = require('gulp-minify-css');
 
 gulp.task('modernizr', function () {
     gulp.src('./bower_components/modernizr/modernizr.js')
@@ -21,19 +19,6 @@ gulp.task('modernizr', function () {
         .pipe(gulp.dest('./js'));
 });
 
-gulp.task('scripts', function () {
-    gulp.src([
-        './bower_components/jquery/dist/jquery.js',
-        './src/js/*.js'
-        ])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./js'));
-});
 
 gulp.task('styles', function() {
     gulp.src('./src/scss/*.scss')
@@ -43,7 +28,6 @@ gulp.task('styles', function() {
             sass: 'src/scss',
             image: 'img'
         }))
-        .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(gulp.dest('./css'))
         .pipe(rename({suffix: '.min'}))
@@ -52,9 +36,25 @@ gulp.task('styles', function() {
 });
 
 
-gulp.task('watch', function() {
-    gulp.watch('src/js/**/*.js', ['scripts'] );
-    gulp.watch('src/scss/**/*.scss', ['styles'] );
+gulp.task('scripts', function () {
+    gulp.src([
+        './bower_components/jquery/dist/jquery.js',
+        './src/js/*.js'
+        ])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./js'));
 });
 
-gulp.task('default', ['scripts', 'styles']);
+
+gulp.task('watch', function() {
+    gulp.watch('./src/js/**/*.js', ['scripts'] );
+    gulp.watch('./src/scss/**/*.scss', ['styles'] );
+});
+
+
+gulp.task('default', ['styles', 'scripts', 'modernizr']);
